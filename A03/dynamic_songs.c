e2fa9daea222f4e5759799c14751cc26e68e3799
Megan Louie <megan.may.louie@gmail>
2024-09-20 17:09:54 -0400

    Assignment 3 completed

--- A03/dynamic_songs.c
+++ A03/dynamic_songs.c
@@ -19,9 +19,13 @@ struct songList{
   float valence; //describes the musical positiveness conveyed by a trac
 };
 
+/*
+*opens and reads in csv file
+*/
 struct songList* readSongFile (){
   FILE *infile; 
   infile = fopen("songlist.csv", "r");
+  // if nothing in the file then exit out of loop
   if (infile == NULL){
     printf("Error: unable to open the file %s\n", "songlist.csv");
     exit(1);
@@ -32,27 +36,37 @@ struct songList* readSongFile (){
   int songNumber = atoi (token);
   fgets(buffer, 100, infile);
   struct songList* list = malloc(songNumber * sizeof(struct songList));
+  //loops through each songs
   for(int i = 0; i < songNumber; i++){
+    //Read a line from the input file into the 'buffer'
     fgets(buffer, 100, infile);
     char* attribute = strtok(buffer, ",");
+    //Copy the song title to the 'song' field of the 'list' array at the current index
+    //does this for all attributes in the file
     strcpy(list[i].song, attribute);
     attribute = strtok(NULL,",");
     strcpy(list[i].artist, attribute);
     attribute = strtok(NULL,",");
     int durMS = atoi (attribute);
+    //Convert the duration from milliseconds to seconds
+    //stores the duration values in their respective places
     durMS = durMS/1000;
     list[i].durationSec = durMS % 60;
     list[i].durationMin = durMS/60;
     attribute = strtok(NULL,",");
+    //Extract and convert the next attribute (danceability) to a float, then store it
     list[i].danceability = atof(attribute);
     attribute = strtok(NULL,",");
+    //Extract and convert the next attribute (energy) to a float, then sto
     list[i].energy = atof(attribute);
     attribute = strtok(NULL,",");
+    //Extract and convert the next attribute (tempo) to a float, then store it
     list[i].tempo = atof(attribute);
+    //Extract and convert the next attribute (valence) to a float, then store it
     attribute = strtok(NULL,",");
     list[i].valence = atof(attribute);
   }
-  fclose(infile);
+  fclose(infile); //Closes the input file after reading all the data
   return list;
 }
 
@@ -75,7 +89,7 @@ void printSongs(struct songList* songs, int numSongs) {
 
 int main() {
   printf("Welcome to Megan's Dynamic Song List! \n");
-  struct songList* list= readSongFile();
+  struct songList* list = readSongFile();
   printSongs(list, 18);
   free(list);
   return 0;
